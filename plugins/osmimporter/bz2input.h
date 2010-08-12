5414b255604871d19043caf67ba12648369eddbe
veaac.fdirct <devnull@localhost>
2010-08-12 08:49:26 +0000

    OSMI: Added support for bz2 files with multiple streams. Should work with multithreaded packers.

--- plugins/osmimporter/bz2input.h
+++ plugins/osmimporter/bz2input.h
@@ -24,43 +24,81 @@ along with MoNav.  If not, see <http://www.gnu.org/licenses/>.
 #include <string.h>
 #include <bzlib.h>
 #include <libxml/xmlreader.h>
+#include <QtDebug>
 
 struct Context {
 	FILE* file;
 	BZFILE* bz2;
-	bool error;
+	bool closed;
+	int error;
+	unsigned char unused[BZ_MAX_UNUSED];
 };
 
-int readFile( void* pointer, char* buffer, int len )
+static bool nextStream( Context* context )
 {
-	Context* context = (Context*) pointer;
-	if ( len == 0 || context->error )
-		return 0;
+	char* unusedPointer;
+	int numberOfUnused;
 
-	int error = 0;
-	int read = BZ2_bzRead( &error, context->bz2, buffer, len );
-	if ( error == BZ_OK )
-		return read;
+	BZ2_bzReadGetUnused( &context->error, context->bz2, (void**) &unusedPointer, &numberOfUnused );
+	if ( context->error != BZ_OK )
+		return false;
 
-	context->error = true;
-	if ( error == BZ_STREAM_END )
-		return read;
-	return 0;
+	for ( int i = 0; i < numberOfUnused; i++ )
+		context->unused[i] = unusedPointer[i];
+
+	BZ2_bzReadClose( &context->error, context->bz2 );
+	if ( context->error != BZ_OK )
+		return false;
+
+	context->bz2 = BZ2_bzReadOpen( &context->error, context->file, 0, 0, context->unused, numberOfUnused );
+	if ( context->error != BZ_OK || context->bz2 == NULL )
+		return false;
+
+	return true;
 }
 
-int inputClose( void *pointer )
+static int readFile( void* pointer, char* buffer, int len )
 {
 	Context* context = (Context*) pointer;
-	BZ2_bzclose( context->bz2 );
+	if ( len == 0 || context->closed )
+		return 0;
+
+	int read = 0;
+	while( read != len ) {
+		read += BZ2_bzRead( &context->error, context->bz2, buffer + read, len - read );
+		if ( context->error == BZ_OK )
+			break;
+
+		if ( context->error == BZ_STREAM_END ) {
+			if ( !nextStream( context ) ) {
+				context->closed = true;
+				break;
+			}
+			continue;
+		}
+
+		context->closed = true;
+
+		if ( context->error == BZ_UNEXPECTED_EOF )
+			return read;
+		return 0;
+	}
+	return read;
+}
+
+static int inputClose( void *pointer )
+{
+	Context* context = (Context*) pointer;
+	BZ2_bzReadClose( &context->error, context->bz2 );
 	fclose( context->file );
 	delete context;
 	return 0;
 }
 
-xmlTextReaderPtr getBz2Reader( const char* name )
+static xmlTextReaderPtr getBz2Reader( const char* name )
 {
 	Context* context = new Context;
-	context->error = false;
+	context->closed = false;
 	context->file = fopen( name, "r" );
 	int error;
 	context->bz2 = BZ2_bzReadOpen( &error, context->file, 0, 0, NULL, 0 );
