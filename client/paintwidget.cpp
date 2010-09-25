38f60fb8f8a0e5a2e8e53ff9d4174fde153cd77e
veaac.fdirct <devnull@localhost>
2010-09-25 20:04:28 +0000

    Client: Small bugfix

--- client/paintwidget.cpp
+++ client/paintwidget.cpp
@@ -24,6 +24,7 @@ along with MoNav.  If not, see <http://www.gnu.org/licenses/>.
 #include <QMouseEvent>
 #include <QWheelEvent>
 #include <QtDebug>
+#include <algorithm>
 
 PaintWidget::PaintWidget(QWidget *parent) :
 	QWidget( parent ),
