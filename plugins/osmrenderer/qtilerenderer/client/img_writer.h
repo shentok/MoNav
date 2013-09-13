08e04fc34819a8daa094a3796ffc01df5bc444d3
Bernhard Beschow <bbeschow@cs.tu-berlin.de>
2013-09-13 18:44:44 +0200

    make private

--- plugins/osmrenderer/qtilerenderer/client/img_writer.h
+++ plugins/osmrenderer/qtilerenderer/client/img_writer.h
@@ -43,14 +43,17 @@ class ImgWriter {
     {
         rasterizer.clip_box(0, 0, x, y);
     }
+
     ~ImgWriter() {
         delete [] buf;
     }
+
     void SetBG(int r, int g, int b) {
         rbase.clear(agg::rgba8(r, g, b));
         //prim->fill_color(agg::rgba8(r, g, b));
         //prim->solid_rectangle(0, 0, frame_width, frame_height);
-    };
+    }
+
     void SetPen(int r, int g, int b, double w=1) {
         if(r==_r && g==_g && b==_b && w==pen_width) return;
 
@@ -59,9 +62,64 @@ class ImgWriter {
         _r = r; _g = g; _b = b;
         line_color = agg::rgba8(r, g, b);
         pen_width = w;
-    };
-    
-    bool clip_line_to_img(int &x1, int &y1, int &x2, int &y2) {
+    }
+
+    void FillPoly(struct coord *points, int npoints) {
+        if(npoints<2) return;
+        if(!polygon_crosses_tile(points, npoints)) return;
+        if(drawing==LINES) draw_lines();
+
+        path.move_to(points[0].x, points[0].y);
+        for(int i=1;i<npoints;i++)
+            path.line_to(points[i].x, points[i].y);
+        path.close_polygon();
+
+        //FIXME Cargo Cult. Is this all really neaded?
+        agg::trans_affine mtx;
+        trans_path_type trans(path, mtx);
+        //stroke_type stroke(trans);
+        agg::conv_close_polygon<trans_path_type> poly(trans);
+
+        renderer.color(line_color);
+        rasterizer.add_path(poly);
+        agg::render_scanlines(rasterizer, scanline, renderer);
+        rasterizer.reset();
+        path.free_all();
+    }
+
+    void DrawLine(int x1, int y1, int x2, int y2) {
+        if(!clip_line_to_img(x1, y1, x2, y2)) return;
+        path.move_to(x1, y1);
+        path.line_to(x2, y2);
+        drawing = LINES;
+    }
+
+    void Save() {
+        if(drawing==LINES) draw_lines();
+        FILE* fd = fopen("tmp.ppm", "wb");
+        if(fd)
+        {
+            fprintf(fd, "P6 %d %d 255 ", frame_width, frame_height);
+            fwrite(buf, 1, frame_width * frame_height * 3, fd);
+            fclose(fd);
+          
+        }
+        std::string s("convert tmp.ppm ");
+        s+= name;
+#ifdef _CRT_SYSTEM_DEFINED // not always defined, e.g. windows mobile
+		  if(std::system(s.c_str())!=0) printf("Failed convert\n");
+#else
+		  printf("System not available, Failed!\n" );
+#endif
+    }
+
+    const unsigned char * get_img_data() {
+        if(drawing==LINES) draw_lines();
+        return buf;
+    }
+
+  private:
+    bool clip_line_to_img(int &x1, int &y1, int &x2, int &y2) const {
         //Simple clipping function for speed. Check profiling
         //to see how much is important here. Return false if line
         //doesn't cross image at all. Otherwise move line ends to
@@ -70,13 +128,13 @@ class ImgWriter {
 
         bool need_adjust_start=true, need_adjust_end=true;
         //Are the ends in the image. This is cheap to test and true for
-        //most lines. 
+        //most lines.
         if((x1>=-5 && x1<=frame_width+5 && y1>=-5 && y1<=frame_height+5)) {
-            if(x2>=-5 && x2<=frame_width+5 && y2>=-5 && y2<=frame_height+5) 
+            if(x2>=-5 && x2<=frame_width+5 && y2>=-5 && y2<=frame_height+5)
                 return true; //Line already contained by image. All done.
             need_adjust_start=false;
         }
-        else if(x2>=-5 && x2<=frame_width+5 && y2>=-5 && y2<=frame_height+5) 
+        else if(x2>=-5 && x2<=frame_width+5 && y2>=-5 && y2<=frame_height+5)
             need_adjust_end=false;
         else {
             //Quick check for lines which are way off.
@@ -128,7 +186,7 @@ class ImgWriter {
             intersect = true;
         }
         if(iright  > -5 && iright  < frame_height+5) {
-            if(x2>x1 && need_adjust_end) {x2=frame_width+5; y2=iright; 
+            if(x2>x1 && need_adjust_end) {x2=frame_width+5; y2=iright;
                                           need_adjust_end=false;}
             if(x1>x2 && need_adjust_end) {x1=frame_width+5; y1=iright;
                                           need_adjust_start=false;}
@@ -136,8 +194,9 @@ class ImgWriter {
         }
         //otherwise we don't
         return intersect;
-    };
-    bool polygon_crosses_tile(struct coord *points, int npoints) {
+    }
+
+    bool polygon_crosses_tile(struct coord *points, int npoints) const {
         //As above. Could be made more accurate, not sure it's worth it.
         int maxx=-10000, maxy=-10000, minx=10000, miny=10000;
         for(int i=0;i<npoints;i++) {
@@ -149,29 +208,8 @@ class ImgWriter {
         if(minx > frame_width || miny > frame_height ||
            maxx < 0 || maxy < 0) return false;
         return true;
-    };
-    void FillPoly(struct coord *points, int npoints) {
-        if(npoints<2) return;
-        if(!polygon_crosses_tile(points, npoints)) return;
-        if(drawing==LINES) draw_lines();
+    }
 
-        path.move_to(points[0].x, points[0].y);
-        for(int i=1;i<npoints;i++)
-            path.line_to(points[i].x, points[i].y);
-        path.close_polygon();
-
-        //FIXME Cargo Cult. Is this all really neaded?
-        agg::trans_affine mtx;
-        trans_path_type trans(path, mtx);
-        //stroke_type stroke(trans);
-        agg::conv_close_polygon<trans_path_type> poly(trans);
-
-        renderer.color(line_color);
-        rasterizer.add_path(poly);
-        agg::render_scanlines(rasterizer, scanline, renderer);
-        rasterizer.reset();
-        path.free_all();
-    };
     void draw_lines() {
         drawing = NOTHING;
         //FIXME Cargo Cult. Is this all really neaded?
@@ -186,37 +224,8 @@ class ImgWriter {
         agg::render_scanlines(rasterizer, scanline, renderer);
         rasterizer.reset();
         path.free_all();
-    };
-    void DrawLine(int x1, int y1, int x2, int y2) {
-        if(!clip_line_to_img(x1, y1, x2, y2)) return;
-        path.move_to(x1, y1);
-        path.line_to(x2, y2);
-        drawing = LINES;
-    };
-    void Save() {
-        if(drawing==LINES) draw_lines();
-        FILE* fd = fopen("tmp.ppm", "wb");
-        if(fd)
-        {
-            fprintf(fd, "P6 %d %d 255 ", frame_width, frame_height);
-            fwrite(buf, 1, frame_width * frame_height * 3, fd);
-            fclose(fd);
-          
-        }
-        std::string s("convert tmp.ppm ");
-        s+= name;
-#ifdef _CRT_SYSTEM_DEFINED // not always defined, e.g. windows mobile
-		  if(std::system(s.c_str())!=0) printf("Failed convert\n");
-#else
-		  printf("System not available, Failed!\n" );
-#endif
-    };
-    const unsigned char * get_img_data() {
-        if(drawing==LINES) draw_lines();
-        return buf;
-    };
+    }
 
-  private:
     agg::scanline_u8 scanline;
     agg::rasterizer_scanline_aa<> rasterizer;
     const std::string name;
